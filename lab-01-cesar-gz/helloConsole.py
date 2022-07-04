fd15c553a556ea09770b1953e4965efcad9957aa
Cesar Gutierrez <cesar.gz@yahoo.com>
2022-07-04 08:51:35 -0700

    Add files via upload

---
+++ lab-01-cesar-gz/helloConsole.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Cesar Gutierrez
+CPSC 223P-01
+1/26/2022
+cesarg7@csu.fullerton.edu
+"""
+
+# This is my first Python program. It greets the whole world.
+print("Hello, world!")
